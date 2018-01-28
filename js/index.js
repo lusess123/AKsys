fadd19ac9e66a3dd189d1ec35b516f64a64be299
zyking <zyking@zykingdeMacBook-Pro.local>
2018-01-28 23:48:52 +0800

    1.0.9

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 var core = require("./Core");
 exports.core = core;
@@ -12,4 +15,10 @@ var vue = require("./Vue");
 exports.vue = vue;
 var util = require("./Util");
 exports.util = util;
-require("./com");
+__export(require("./com"));
+__export(require("./com/BaseCom"));
+__export(require("./event"));
+__export(require("./event/VueEvent"));
+__export(require("./net"));
+__export(require("./net/code"));
+__export(require("./net/net"));
