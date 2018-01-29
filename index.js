816bd060b59cc70b8d329992079d7110db73da92
zyking　 <zyking@zykingdeMacBook-Pro-2.local>
2018-01-29 15:41:29 +0800

    1.1.1

--- index.js
+++ index.js
@@ -12,12 +12,3 @@ var vue = require("./Vue");
 exports.vue = vue;
 var util = require("./Util");
 exports.util = util;
-// export * from "./com";
-// export * from "./com/BaseCom";
-// export * from "./com/ICom";
-// export * from "./event"
-// export * from "./event/IEvent"
-// export * from "./event/VueEvent"
-// export * from "./net"
-// export * from "./net/code"
-// export * from "./net/net" 
