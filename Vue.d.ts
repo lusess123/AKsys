0686b06d73e150e25071e16b551211528a032327
zyking　 <zyking@zykingdeMacBook-Pro-2.local>
2018-02-05 14:43:10 +0800

    合并更改

--- Vue.d.ts
+++ Vue.d.ts
@@ -3,8 +3,5 @@ export declare const vm: (objPro: string) => string;
 export declare const registAndGetVueComName: (vm: any, vueObj?: any) => string;
 export declare const vueTpl: (name: string, components?: any, comOpt?: any) => (tpl: string) => any;
 export declare const com: (vue: any, comOpt?: any) => (constructor: Function) => void;
-<<<<<<< HEAD
 export declare function getTempVueName(vueProty: any, name?: string): string;
-=======
 export declare const compute: (name?: string) => (target: any, propertyKey: string, descriptor: PropertyDescriptor) => void;
->>>>>>> efd8649d64d38c3c8d85032eb005604f37e41dd0
