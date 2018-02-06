ad74bf8dd39c24665e18274d84db6907e95507d2
zyking　 <zyking@zykingdeMacBook-Pro-2.local>
2018-02-06 10:52:35 +0800

    vue forceupdate

--- src/Vue.ts
+++ src/Vue.ts
@@ -13,37 +13,37 @@ const _com = function (h: any, name: string, tpl: string, pro: any, props: strin
 
     Vue.component(name, _vueObj);
 
-    return h(name, { props: {vm:pro} });
+    return h(name, { props: { vm: pro } });
 }
 
 export const tpl = h =>
     (tpl: string, pro?: any) => {
-        
-        return _com(h, "tplName"+ core.getUniId(), tpl, pro?pro:{}, ["vm"])
+
+        return _com(h, "tplName" + core.getUniId(), tpl, pro ? pro : {}, ["vm"])
 
     }
-export const vm = (objPro :string)=>{
+export const vm = (objPro: string) => {
 
-   return `
+    return `
        <temple   v-if="vm.${objPro} && vm.${objPro}.render"  :is="vm.${objPro}.render()"  :vm="vm.${objPro}"></temple>
-   `; 
+   `;
 }
 
 
 export const registAndGetVueComName = (vm, vueObj?) => {
     //debugger ;
     if (!vueObj) {
-        if(vm){
-            if(vm._VueObj){
+        if (vm) {
+            if (vm._VueObj) {
                 vueObj = vm._VueObj;
             }
-            else{
+            else {
                 if (vm["constructor"]["_vueObj"]) {
                     vueObj = vm["constructor"]["_vueObj"];
                 }
             }
         }
-        
+
     }
     if (!vueObj) throw { error: "组件不能为空", vm: vm };
     let _name: string = "";
@@ -51,12 +51,12 @@ export const registAndGetVueComName = (vm, vueObj?) => {
         return _name = "tempvuecom";
     }
     else {
-       // debugger;
-        if(vm["constructor"]){
-          _name = util.getFunName(vm["constructor"]);
+        // debugger;
+        if (vm["constructor"]) {
+            _name = util.getFunName(vm["constructor"]);
         }
-        else{
-        _name = vueObj.name;
+        else {
+            _name = vueObj.name;
         }
     }
     _name = _name + core.getUniId();
@@ -67,104 +67,129 @@ export const registAndGetVueComName = (vm, vueObj?) => {
 }
 
 export const vueTpl =
-    (name: string,components?:any,comOpt?:any ) =>
+    (name: string, components?: any, comOpt?: any) =>
         (tpl: string) => {
-            
-          //  const _vueObj = Vue.extend(
-             const _vueOpt =   {
+
+            //  const _vueObj = Vue.extend(
+            const _vueOpt = {
+                data(){
+                    return {
+                          _forceUpdateFun:null
+                    };
+                },
                 name: name,
                 props: ["vm"],
                 template: tpl,
-                components:components,
-                renderError:  (h:any, err)=> {
-                    return h('pre', {title:err.stack, style: { color: 'red' }}, err.stack);
-                  },
-                  beforeCreated:function(){
-                    this.vm.$store = this.$store ;
-                  },
+                components: components,
+                renderError: (h: any, err) => {
+                    return h('pre', { title: err.stack, style: { color: 'red' } }, err.stack);
+                },
+                beforeCreated: function () {
+                    this.vm.$store = this.$store;
+                },
 
-                  created:function(){
-                    this.vm.$store = this.$store ;
-                  },
-                   
-                 beforeUpdate:function(){
-                      this.vm.$store = this.$store ;
-                 },
+                created: function () {
+                    this.vm.$store = this.$store;
+                },
 
-                  mounted: function () {
-                     // alert("控件完成了");
-                  this.vm.$store = this.$store ;
-                      if(this.$props.vm){
-                          if(this.$props.vm.getEvent){
-                              const _event = this.$props.vm.getEvent();
-                              const me = this ;
-                              _event.on("forceUpdate",function(){
-                                 // alert("页面组件更新");
-                                 // debugger ;
-                                  me.$forceUpdate();
-                              });
-                          }
-                      }
-                  }
+                beforeUpdate: function () {
+                    this.vm.$store = this.$store;
+                },
+
+                watch: {
+
+                    vm(newVm) {
+                        if (newVm) {
+                            const _event = newVm.getEvent();
+                            const me = this;
+                            if(this._forceUpdateFun){
+                                _event.off("forceUpdate", this._forceUpdateFun);
+                            }
+                            this._forceUpdateFun = ()=>{
+                                me.$forceUpdate();
+                            };
+                            _event.on("forceUpdate", this._forceUpdateFun);
+                        }
+                    }
+
+                },
+
+                mounted: function () {
+                    // alert("控件完成了");
+                    this.vm.$store = this.$store;
+                    if (this.$props.vm) {
+                        if (this.$props.vm.getEvent) {
+                            const _event = this.$props.vm.getEvent();
+                            const me = this;
+                            if(this._forceUpdateFun){
+                                _event.off("forceUpdate", this._forceUpdateFun);
+                            }
+                            this._forceUpdateFun = ()=>{
+                                me.$forceUpdate();
+                            };
+                            _event.on("forceUpdate", this._forceUpdateFun);
+                        }
+                    }
+                }
             }
 
-            comOpt = comOpt?comOpt : {} ;
+            comOpt = comOpt ? comOpt : {};
 
-           comOpt = {..._vueOpt,...comOpt};
-         
-      //  );
-           const _obj:any = Vue.extend(comOpt);
+            comOpt = { ..._vueOpt, ...comOpt };
+
+            //  );
+            const _obj: any = Vue.extend(comOpt);
             return _obj;
         }
 
-    
 
-export const com = function (vue: any,comOpt:any = {} ) {
+
+export const com = function (vue: any, comOpt: any = {}) {
     return function (constructor: Function) {
         //debugger ;
-        comOpt = { ...comOpt,...{extends:constructor["_vueObj"]}};
-        const  components =  comOpt.components ;
+        comOpt = { ...comOpt, ...{ extends: constructor["_vueObj"] } };
+        const components = comOpt.components;
         const _type = typeof (vue);
         if (_type == "function")
             constructor["_vueObj"] = vue;
         else {
             if (_type == "string")
-                constructor["_vueObj"] = vueTpl(util.getFunName(constructor) + core.getUniId(),components,comOpt)(vue);
+                constructor["_vueObj"] = vueTpl(util.getFunName(constructor) + core.getUniId(), components, comOpt)(vue);
             else {
-                const _base = {name : "com" + core.getUniId() , props: ["vm"]};
-                constructor["_vueObj"] = Vue.extend( {..._base, ...vue,...comOpt} );
+                const _base = { name: "com" + core.getUniId(), props: ["vm"] };
+                constructor["_vueObj"] = Vue.extend({ ..._base, ...vue, ...comOpt });
             }
         }
     }
 }
 
-export function  getTempVueName(vueProty : any,name ?:string){
-          let _name = name ;
-          if(!_name){
-                _name =   util.getFunName(vueProty);
-          }
-          _name = _name + event.getUniId();
-          return _name ;
+export function getTempVueName(vueProty: any, name?: string) {
+    let _name = name;
+    if (!_name) {
+        _name = util.getFunName(vueProty);
+    }
+    _name = _name + event.getUniId();
+    return _name;
 }
 
 
-export const compute  =  function (name?:string ) {
+export const compute = function (name?: string) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         const _baseVue = target.constructor["_vueObj"];
-        if(_baseVue){
+        if (_baseVue) {
             //constructor["_vueObj"]  = 
-            target.constructor["_vueObj"] ={
-                      computed:{
-                           [name?name:propertyKey]:function(){
-                              return  this.vm[propertyKey];
-                          }
-                        },
-                        
-                        extends:_baseVue
+            target.constructor["_vueObj"] = {
+                computed: {
+                    [name ? name : propertyKey]: function () {
+                        return this.vm[propertyKey];
+                    }
+                },
 
-                    };
-            
-                
+                extends: _baseVue
+
+            };
+
+
         }
     };
 }
